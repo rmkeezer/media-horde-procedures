feb9b8b0a3ea7bf72da21277414f84e77d1fe153
rmkeezer <rmkeezer@yahoo.com>
2017-07-24 00:39:27 -0500

    Inital commit with readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Introduction
+============
+
+This SQL file contains the procedures needed for [media-horde-api](https://github.com/rmkeezer/media-horde-api)  
+
+It was created using:  
+```mysqldump -n -t -d -R <dbname>```
---
+++ mediaHordeDump.sql
@@ -0,0 +1,411 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mediahorde
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping routines for database 'mediahorde'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `spAddRow` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spAddRow`(
+IN p_TableName varchar(50),
+IN p_ArgNames varchar(2000),
+IN p_ArgVals varchar(2000)
+)
+BEGIN
+
+SET @add_row = concat('INSERT INTO ',
+  p_TableName,
+  '(',
+  p_ArgNames,
+  ')',
+  'values(',
+  p_ArgVals,
+  ');');
+
+PREPARE stmt FROM @add_row;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!50003 DROP PROCEDURE IF EXISTS `spAuthenticateUser` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spAuthenticateUser`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+
+     select * from tblUser where UserName = p_username;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!50003 DROP PROCEDURE IF EXISTS `spCreateTable` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateTable`(
+IN p_TableName varchar(50),
+IN p_Arguments varchar(10000)
+)
+BEGIN
+
+SET @create_table = concat('CREATE TABLE IF NOT EXISTS `mediahorde`.`',p_TableName,'` (
+  `Id` INT NOT NULL AUTO_INCREMENT,'
+  ,p_Arguments,
+  'PRIMARY KEY (`Id`));');
+
+PREPARE stmt FROM @create_table;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!50003 DROP PROCEDURE IF EXISTS `spCreateUser` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateUser`(
+IN p_Username varchar(50),
+IN p_Password varchar(50)
+)
+BEGIN
+
+if ( select exists (select 1 from tblUser where UserName = p_username) ) THEN
+
+    select 'Username already exists, please choose another';
+
+ELSE
+
+insert into tblUser
+(
+    UserName,
+    Password
+)
+values
+(
+    p_Username,
+    p_Password
+);
+
+END IF;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!50003 DROP PROCEDURE IF EXISTS `spGetAllRows` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetAllRows`(
+IN p_TableName varchar(50)
+)
+BEGIN
+
+SET @get_all_rows = concat('SELECT * FROM ',
+  p_TableName);
+
+PREPARE stmt FROM @get_all_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!50003 DROP PROCEDURE IF EXISTS `spGetJoinedRowsOrdered` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetJoinedRowsOrdered`(
+IN p_Table1 varchar(50),
+IN p_Table2 varchar(50),
+IN p_Join1 varchar(50),
+IN p_Join2 varchar(50),
+IN p_JoinType varchar(50),
+IN p_Null varchar(50),
+IN p_Neg varchar(50),
+IN p_Offset varchar(200),
+IN p_NumRows varchar(200),
+IN p_Order varchar(200),
+IN p_Dir varchar(50)
+)
+BEGIN
+
+SET @get_rows = concat('SELECT * FROM ',
+  p_Table1,
+  ' ',
+  p_JoinType,
+  ' JOIN ',
+  p_Table2,
+  ' ON ',
+  p_Table1,
+  '.',
+  p_Join1,
+  ' = ',
+  p_Table2,
+  '.',
+  p_Join2,
+  ' WHERE ',
+  p_Null,
+  ' IS ',
+  p_Neg,
+  ' NULL ',
+  ' ORDER BY ',
+  p_Order,
+  ' ',
+  p_Dir,
+  ' LIMIT ',
+  p_NumRows,
+  ' OFFSET ',
+  p_Offset,
+  ';');
+
+PREPARE stmt FROM @get_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spGetRows` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetRows`(
+IN p_TableName varchar(50),
+IN p_Offset varchar(200),
+IN p_NumRows varchar(200)
+)
+BEGIN
+
+SET @get_rows = concat('SELECT * FROM ',
+  p_TableName,
+  ' LIMIT ',
+  p_NumRows,
+  ' OFFSET ',
+  p_Offset,
+  ';');
+
+PREPARE stmt FROM @get_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spGetRowsOrdered` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetRowsOrdered`(
+IN p_TableName varchar(50),
+IN p_Offset varchar(200),
+IN p_NumRows varchar(200),
+IN p_Order varchar(200),
+IN p_Dir varchar(50)
+)
+BEGIN
+
+SET @get_rows = concat('SELECT * FROM ',
+  p_TableName,
+  ' ORDER BY ',
+  p_Order,
+  ' ',
+  p_Dir,
+  ' LIMIT ',
+  p_NumRows,
+  ' OFFSET ',
+  p_Offset,
+  ';');
+
+PREPARE stmt FROM @get_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spGetXRandRows` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetXRandRows`(
+IN p_TableName varchar(50),
+IN p_Offset varchar(50),
+IN p_NumRows varchar(50)
+)
+BEGIN
+
+SET @get_x_rand_rows = concat('SELECT * FROM ',
+  p_TableName,
+  ' ORDER BY RAND() LIMIT ',
+  p_NumRows,
+  ' OFFSET ',
+  p_Offset);
+
+PREPARE stmt FROM @get_x_rand_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spUpdateRows` */;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spUpdateRows`(
+IN p_TableName varchar(50),
+IN p_Changes varchar(2000),
+IN p_Ids varchar(2000)
+)
+BEGIN
+
+SET @update_rows = concat('UPDATE ',
+  p_TableName,
+  ' SET ',
+  p_Changes,
+  ' WHERE Id in (',
+  p_Ids,
+  ');');
+
+PREPARE stmt FROM @update_rows;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `mediahorde` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-24  0:31:34
